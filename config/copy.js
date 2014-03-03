1e2bb8ad3b3d4a930c9c6d49e1e037bb379fd5c7
Tri Nguyen <tri@tridnguyen.com>
2014-03-02 19:22:08 -0500

    -n clean up copy task

--- config/copy.js
+++ config/copy.js
@@ -1,8 +1,6 @@
 module.exports = {
 	build: {
 		files: [
-			{expand: true, src: ['CNAME'], dest: '<%= buildPath %>/'},
-			{expand: true, cwd: 'components', src: ['fancybox/source/**/*'], dest: '<%= buildPath %>/components'},
 			{expand: true, cwd: 'sass', src: 'assets/**/*', dest: '<%= buildPath %>/css/'}
 		]
 	}
