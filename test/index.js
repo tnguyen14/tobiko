33f54b97c6d588d4cb8586c349557c87ef983091
Tri Nguyen <tri@tridnguyen.com>
2015-05-16 18:40:47 -0400

    refactor tests

--- test/index.js
+++ test/index.js
@@ -45,35 +45,36 @@ test('decorate file', function(t) {
 	t.equal(file.url, '/fixtures/foo', 'File url');
 });
 
-test('create paginated archive', function(t) {
-	t.plan(2);
-	var postsObject = {
-		postOne: {
-			'index.json': {
-				template: 'post.hbs',
-				order: 1
-			}
-		},
-		postTwo: {
-			'index.md': {
-				template: 'post.hbs',
-				order: 3
-			}
-		},
-		postThree: {
-			'index.json': {
-				template: 'post.hbs',
-				order: 2
-			}
+var archivePosts = {
+	postOne: {
+		'index.json': {
+			template: 'post.hbs',
+			order: 1
 		}
-	};
-	var postsOption = {
-		postPerPage: 2,
-		template: 'archive.hbs',
-		title: 'Posts',
-		orderby: 'order'
-	};
-	var posts = archive.getPosts(postsObject, postsOption.orderby);
+	},
+	postTwo: {
+		'index.md': {
+			template: 'post.hbs',
+			order: 3
+		}
+	},
+	postThree: {
+		'index.json': {
+			template: 'post.hbs',
+			order: 2
+		}
+	}
+};
+var archiveOption = {
+	postPerPage: 2,
+	template: 'archive.hbs',
+	title: 'Posts',
+	orderby: 'order'
+};
+
+test('Get and sort posts for archive', function(t) {
+	t.plan(1);
+	var posts = archive.getPosts(archivePosts, archiveOption.orderby);
 	t.deepEqual(posts, [{
 		order: 1,
 		template: 'post.hbs'
@@ -84,14 +85,17 @@ test('create paginated archive', function(t) {
 		order: 3,
 		template: 'post.hbs'
 	}], 'Get and sort posts for archive');
+});
 
-	var archivePages = archive.paginate(postsObject, 'posts', postsOption);
+test('Generate archive pages', function(t) {
+	t.plan(1);
+	var archivePages = archive.paginate(archivePosts, 'posts', archiveOption);
 	t.deepEqual(archivePages, {
 		'index.html': {
 			filepath: 'posts/index.html',
 			url: '/posts',
-			template: postsOption.template,
-			title: postsOption.title,
+			template: archiveOption.template,
+			title: archiveOption.title,
 			prevUrl: '/posts/2',
 			posts: [{
 				order: 1,
@@ -105,8 +109,8 @@ test('create paginated archive', function(t) {
 			'index.html': {
 				filepath: 'posts/index.html',
 				url: '/posts',
-				template: postsOption.template,
-				title: postsOption.title,
+				template: archiveOption.template,
+				title: archiveOption.title,
 				prevUrl: '/posts/2',
 				posts: [{
 					order: 1,
@@ -121,8 +125,8 @@ test('create paginated archive', function(t) {
 			'index.html': {
 				filepath: 'posts/2/index.html',
 				url: '/posts/2',
-				template: postsOption.template,
-				title: postsOption.title,
+				template: archiveOption.template,
+				title: archiveOption.title,
 				nextUrl: '/posts/1',
 				posts: [{
 					order: 3,
