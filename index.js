2d442f3259ec2a40e82609a1249d563859404a84
Tri Nguyen <tri@tridnguyen.com>
2016-10-10 10:55:05 -0400

    generatel html from handlebars templates

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const path = require('path');
 const fs = require('fs');
 const decorate = require('./lib/decorate');
 const parse = require('./lib/parse');
+const getTemplates = require('./lib/templates');
 const glob = require('glob');
 const debug = require('debug')('tobiko');
+const mkdirp = require('mkdirp');
 
 module.exports = function (opts) {
-	importContents(opts);
+	return importContents(opts).then((contentTree) => {
+		return generateHtml(opts, contentTree);
+	});
 }
 
 function importContents (options) {
-	let opts = Object.assign({}, options, {
+	let opts = Object.assign({}, {
 		baseDir: 'contents',
 		markdown: {
 			breaks: true,
@@ -21,26 +25,18 @@ function importContents (options) {
 			archives: {},
 			wordpress: {}
 		}
-	});
+	}, options);
 
 	var contentTree = {};
 
-	glob(opts.baseDir + '/**/*.{md,json}', (err, files) => {
-		files.forEach((filepath) => {
-			processFile(filepath, opts, contentTree);
-		});
-
-		if (options.outFile) {
-			fs.writeFile(options.outFile, JSON.stringify(contentTree, null, '\t'), (err) => {
-				if (err) {
-					debug(err);
-					return;
-				}
-				debug('Finished writing contents to ' + options.outFile);
+	return new Promise((resolve, reject) => {
+		glob(opts.baseDir + '/**/*.{md,json}', (err, files) => {
+			files.forEach((filepath) => {
+				processFile(filepath, opts, contentTree);
 			});
-		} else {
-			console.log(JSON.stringify(contentTree));
-		}
+
+			resolve(contentTree);
+		});
 	});
 }
 
@@ -74,3 +70,89 @@ function processFile (filepath, opts, contentTree) {
 	currentDir[file.filename] = file;
 }
 
+function generateHtml (options, contentTree) {
+	let opts = Object.assign({}, {
+		handlebars: {
+			templatesDir: 'app/templates',
+			partialsDir: 'app/templates/partials',
+			helpersDir: 'app/templates/helpers'
+		}
+	}, options);
+
+	let env = process.env.ENV;
+
+	let config = require(process.cwd() + '/config.json');
+	if (env) {
+		try {
+			fs.statSync(process.cwd() + '/config.' + env + '.json');
+			Object.assign(config, require(process.cwd() + '/config.' + env + '.json'));
+		} catch (e) {
+			debug(e);
+		}
+	}
+	
+	let templates = getTemplates(opts.handlebars);
+
+	let fileTally = 0;
+
+	return renderContent(contentTree)
+		.then(() => {
+			debug('Created ' + fileTally + ' files.');
+		}, (err) => {
+			debug(err);
+		});
+	
+	function renderContent (contents) {
+		return Promise.all(Object.keys(contents).map((filename) => {
+			let file = contents[filename];
+
+			// check if file is an object
+			if (file !== Object(file)) {
+				return;
+			}
+			// keep going deeper into the content tree
+			if (!file.template) {
+				return renderContent(file);
+			}
+			if (!templates[file.template]) {
+				debug('Could not find template ' + file.template + ' for ' + filename);
+				return;
+			}
+			let context = Object.assign({}, contents);
+			let dirname = path.dirname(file.filepath);
+			// expose env
+			file.env = env;
+
+			// expose file content? on the context
+			context.content = file;
+			// expose config on the context
+			context.config = config;
+
+			// make all data available as context's "global"
+			context.global = contents;
+
+			let html = templates[file.template](context);
+
+			if (dirname === '.') {
+				dirname = '';
+			}
+
+			let outPath = path.join(options.outDir, dirname, file.filename + '.html');
+			return new Promise((resolve, reject) => {
+				mkdirp(path.dirname(outPath), (err) => {
+					if (err) {
+						return reject(err);
+					}
+					fs.writeFile(outPath, html, (err) => {
+						if (err) {
+							return reject(err);
+						}
+						debug('"' + outPath + '" was created.');
+						fileTally++;
+						resolve(file.filename);
+					});
+				});
+			});
+		}));
+	}
+}
