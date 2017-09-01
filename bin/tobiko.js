0b480dce663a8ce6f264e01d568cfa7d3c2fda69
Tri Nguyen <tri@tridnguyen.com>
2017-09-01 01:04:32 -0400

    add watch mode, close #26

--- bin/tobiko.js
+++ bin/tobiko.js
@@ -2,6 +2,9 @@
 
 const tobiko = require('../');
 const config = require('@tridnguyen/config');
+const chokidar = require('chokidar');
+const importContents = require('../lib/importContents');
+const generateHtml = require('../lib/generateHtml');
 const argv = require('yargs')
 	.usage('Run tobiko and generate static files!\n\nUsage: $0 [options]')
 	.option('file', {
@@ -9,9 +12,62 @@ const argv = require('yargs')
 		default: 'tobiko.json',
 		describe: 'path to config file'
 	})
+	.option('watch', {
+		alias: 'w',
+		describe: 'watch mode'
+	})
 	.help().alias('help', 'h')
 	.argv;
 
-tobiko(config(argv.file, {
+const options = config(argv.file, {
 	caller: false
-}));
+});
+
+let contentTree, importingContents, generatingHtml, contentTimeout, htmlTimeout;
+
+tobiko(options);
+
+function doImportContents () {
+	if (contentTimeout) {
+		clearTimeout(contentTimeout);
+		contentTimeout = undefined;
+	}
+	if (importingContents) {
+		console.log('Importing in process. Wait...');
+		contentTimeout = setTimeout(doImportContents, 1000);
+		return;
+	}
+	importingContents = true;
+	return importContents(options).then(contents => {
+		contentTree = contents;
+		importingContents = false;
+		contentTimeout = undefined;
+	});
+}
+
+function doGenerateHtml () {
+	if (htmlTimeout) {
+		clearTimeout(htmlTimeout);
+		htmlTimeout = undefined;
+	}
+	if (generatingHtml) {
+		console.log('Generating in process. Wait...');
+		htmlTimeout = setTimeout(doGenerateHtml, 1000);
+		return;
+	}
+	// use the "cached" contentTree to avoid re-importing contents
+	if (contentTree) {
+		generatingHtml = true;
+		generateHtml(options, contentTree).then(() => {
+			generatingHtml = false;
+			htmlTimeout = undefined;
+		});
+	} else {
+		doImportContents().then(doGenerateHtml);
+	}
+}
+
+if (argv.watch) {
+	chokidar.watch(options.contentsDir).on('all', doImportContents);
+	chokidar.watch(options.handlebars.templatesDir).on('all', doGenerateHtml);
+}
